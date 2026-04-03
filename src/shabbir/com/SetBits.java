19c3f10399e3b352dce68af3f3d69d155b974272
Shabbir <shabbirdk05@gmail.com>
2026-04-03 22:59:36 +0530

    "Condition statement for finding digits in given number"

--- src/shabbir/com/SetBits.java
+++ src/shabbir/com/SetBits.java
@@ -4,5 +4,16 @@ public class SetBits {
     static void main(String[] args) {
         int n=45;
         System.out.println(Integer.toBinaryString(n));
+        System.out.println(setBits(n));
+    }
+
+    private static int setBits(int n) {
+        int count=0;
+        while (n>0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+
     }
 }
